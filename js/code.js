ec664ae9a0cb376c82e56e23893abb54d704a1fa
JuukoWK <71335087+JuukoWK@users.noreply.github.com>
2020-12-18 21:53:20 -0500

    first commit

---
+++ js/code.js
@@ -0,0 +1,31 @@
+/*
+let audio = document.getElementById('song2');
+let playPause = document.getElementById('playPauseBTN');
+let count = 0;
+
+function playPause(){
+    if(count == 0){
+        count = 1;
+        audio.play();
+        playPauseBTN.innerhtml= "Pause &#9208;";
+    }else {
+        count = 0;
+        audio.pause();
+        
+    }
+}
+
+function stop(){
+    playPause();
+    audio.pause();
+    
+
+}
+
+I cant figure out why my condtions arent working so ill return to it.
+
+Also all free music is from 
+https://freemusicarchive.org/genre/Jazz
+
+*/
+
